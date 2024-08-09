26dcad182ec4d9bc9ba64f195c9eac7ece6fe77c
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-08-09 22:58:01 +0530

    📱 (feat): auth page setup

--- lib/features/auth/pages/auth.page.dart
+++ lib/features/auth/pages/auth.page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../providers/auth.service.riverpod.dart';
+import '../providers/auth.riverpod.dart';
 
 class AuthPage extends ConsumerStatefulWidget {
   const AuthPage({super.key});
@@ -11,41 +11,58 @@ class AuthPage extends ConsumerStatefulWidget {
 }
 
 class _AuthPageState extends ConsumerState<AuthPage> {
-  Future<void> _signInWithGoogle() async {
-    try {
-      await ref.read(authServiceProvider).signInWithGoogleAndRegisterDatabase();
-    } catch (e) {
-      print(e);
-    }
+  bool booting = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
   }
+
+  _init() async {
+    await ref.read(authProvider.notifier).init();
+    setState(() {
+      booting = false;
+    });
+  }
+  // Future<void> _signInWithGoogle() async {
+  //   try {
+  //     await ref.read(authServiceProvider).signInWithGoogleAndRegisterDatabase();
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-           
-            Positioned(
-              top: MediaQuery.sizeOf(context).height * 0.4,
-              left: 20,
-              child: const Text('Let\'s\nget started'),
+      body: Consumer(
+        builder: (context, ref, _) {
+          if (booting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    await ref.read(authProvider.notifier).continueWithGoogle();
+                  },
+                  child: const Text("Sign In with Google"),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await ref.read(authProvider.notifier).logout();
+                  },
+                  child: const Text("Logout"),
+                ),
+              ],
             ),
-            Positioned(
-              top: MediaQuery.sizeOf(context).height * 0.58,
-              left: 20,
-              child: const Text('Everything start from here'),
-            ),
-            Positioned(
-              top: MediaQuery.sizeOf(context).height * 0.68,
-              left: 40,
-              child: ElevatedButton(
-                onPressed: _signInWithGoogle,
-                child: const Text('Sign in with Google'),
-              ),
-            )
-          ],
-        ),
+          );
+        },
       ),
     );
   }
