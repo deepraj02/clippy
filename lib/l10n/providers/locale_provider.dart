9f380f29fef426c4011d09961fc6297be590a027
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-12-01 15:40:00 +0530

    ♻️ (cleanup): locale & chore cleanup

--- lib/l10n/providers/locale_provider.dart
+++ lib/l10n/providers/locale_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'gen/locale_provider.g.dart';
+
 @riverpod
 class LocaleSettings extends _$LocaleSettings {
   @override
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_auth
 import firebase_core
 import google_sign_in_ios
 import package_info_plus
+import path_provider_foundation
 import sentry_flutter
 import shared_preferences_foundation
 
@@ -21,6 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
